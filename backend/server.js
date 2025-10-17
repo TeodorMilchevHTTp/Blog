58276b228ee66058f41338e5af0bec729c5635aa
Teodor <teodor.v.milchev@gmail.com>
2025-10-17 16:13:41 +0100

    nginx fix

--- backend/server.js
+++ backend/server.js
@@ -13,11 +13,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Serve static files from the React build folder
 app.use(express.static(path.join(__dirname, '..', 'build')));
 
-// GET endpoint for testing
-app.get('/api', (req, res) => {
-  res.send({ message: 'Hello from the backend!' });
-});
-
 // Catch-all to serve React app
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
